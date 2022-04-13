8a901550165a893a4d1322b0603b91de5307d3f6
Ashwin <ahpatel99999@gmail.com>
2022-04-13 11:01:22 +0530

    starting adding productDetail

--- src/components/DropdownFilter/DropdownFilter.js
+++ src/components/DropdownFilter/DropdownFilter.js
@@ -1,15 +1,25 @@
-import './DropdownFilter.css';
-import {
-  DropdownButton,
-  Dropdown
-} from 'react-bootstrap';
+import "./DropdownFilter.css";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
 const DropdownFilter = ({ dropdownData, sortingFilterQueryHandler }) => {
   return (
-    <DropdownButton id="dropdown-basic-button" className='dropdown__btn' title={dropdownData.title}>
-      {dropdownData.items.map(item => <Dropdown.Item key={item.name} onClick={() => sortingFilterQueryHandler(item.query)} className='dropdown__item'>{item.name}</Dropdown.Item>)}
+    <DropdownButton
+      id="dropdown-basic-button"
+      className="dropdown__btn"
+      title={dropdownData.title}
+    >
+      {dropdownData.items.map((item, index) => (
+        <Dropdown.Item
+          // key={item.name}
+          key={index}
+          onClick={() => sortingFilterQueryHandler(item.query)}
+          className="dropdown__item"
+        >
+          {item.name}
+        </Dropdown.Item>
+      ))}
     </DropdownButton>
   );
-}
+};
 
 export default DropdownFilter;
